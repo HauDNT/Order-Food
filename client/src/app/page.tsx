a2570be31ef623f57f82206fdc4fe5c13f03a8bb
Thomas <tienhau.it@gmail.com>
2024-11-10 13:18:38 +0700

    Init: Init project with frontend NextJS

---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <>
+            <h1>Fullstack</h1>
+            <div>
+                <Button type="primary">Button</Button>
+            </div>
+        </>
+    );
+}
