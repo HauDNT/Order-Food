6699ec3509d809ef8c75db29ed3b0623f2b54bc7
Thomas <tienhau.it@gmail.com>
2024-11-11 16:58:52 +0700

    Create: Create new user API, bcrypt helper methods | Update: Update create-user dto

--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -1,26 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { hashPassword } from '@/utils/bcryptPassword.util';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+    constructor(
+        @InjectModel(User.name)
+        private userModel: Model<User>,
+    ) { }
 
-  findAll() {
-    return `This action returns all users`;
-  }
+    async isEmailExist(email: string) {
+        const userIsExist = await this.userModel.exists({ email });
+        return userIsExist ? true : false;
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    async create(createUserDto: CreateUserDto) {
+        const { name, email, password, phone, address, image } = createUserDto;
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+        // Check user exist?
+        const isUserExist = await this.isEmailExist(email);
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+        if (!isUserExist) {
+            // Hash password
+            const passwordHashed = await hashPassword(password);
+
+            const newUser = await this.userModel.create({
+                name, email, password: passwordHashed, phone, address, image
+            });
+
+            return {
+                _id: newUser._id
+            };
+        }
+        else {
+            throw new BadRequestException(`Email ${email} đã tồn tại. Vui lòng sử dụng email khác!`);
+        }
+    }
+
+    async findAll(queryString: string, current: number, pageSize: number) {
+        const { filter, sort } = aqp(queryString);
+
+        if (filter.current) delete filter.current;
+        if (filter.pageSize) delete filter.pageSize;
+
+        if (!current) current = 1;
+        if (!pageSize) pageSize = 10;
+
+        const totalItems = (await this.userModel.find(filter)).length;
+        const totalPages = Math.ceil(totalItems / pageSize);
+        const offset = (current - 1) * (pageSize);
+
+        const results = await this.userModel
+            .find(filter)
+            .limit(pageSize)
+            .skip(offset)
+            .select("-password")
+            .sort(sort as any);
+
+        return { results, totalPages };
+    }
+
+    findOne(id: number) {
+        return `This action returns a #${id} user`;
+    }
+
+    update(id: number, updateUserDto: UpdateUserDto) {
+        return `This action updates a #${id} user`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} user`;
+    }
 }
