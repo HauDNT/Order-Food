7e3a0991f6b178ffeffd8e9afc78fbba422f1f45
Thomas <tienhau.it@gmail.com>
2024-11-11 15:55:57 +0700

    Create: Create and config launch.json file to debug

--- server/src/modules/users/dto/create-user.dto.ts
+++ server/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { IsNotEmpty } from "class-validator";
+
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    name: string;
+    email: string;
+    password: string;
+    phone: string;
+    address: string;
+    image: string;
+    role: string;
+}
